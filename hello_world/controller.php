26d1a8520aab832ee93fb22da742a48bce1473bb
Aderson Medeiros <Aderson Medeiros>
2017-06-24 15:57:44 -0300

    Alterar controle para converter de BSON para JSON

--- hello_world/controller.php
+++ hello_world/controller.php
@@ -1,11 +1,15 @@
 <?php
   $mongo = new MongoDB\client("mongodb://admin:admin@localhost:27017");
   $collection = $mongo->local->teste;
-  // $result = $collection->find();
-
-  $insert = $collection->insertOne([
-
-    "filho" => [$_GET['f1'], $_GET['f2']]
-
-  ]);
+  $result = $collection->find();
+  // $insert = $collection->insertOne([
+  //
+  //   "filho" => [$_GET['f1'], $_GET['f2']]
+  //
+  // ]);
+  $array = array();
+  foreach ($result as $key=>$id) {
+    $array[] = $id['filho'];
+  }
+  print_r(json_encode($array));
 ?>
